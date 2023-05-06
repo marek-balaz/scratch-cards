3bfaabbd215d98901b3165eef39e30c0a86572ef
Marek Baláž <marek.balaz@casperise.com>
2023-05-07 01:27:05 +0200

    Initial Commit

---
+++ ScratchCards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScratchCards
+//
+//  Created by Marek Baláž on 07/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
